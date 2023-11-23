1698dc583dec4c20cba818796585940f963cb108
BUBA <buba86@ro.ru>
2023-11-23 22:36:51 +0500

    2 задача 2 ДЗ

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -13,19 +13,54 @@ function getArrayParams(...arr) {
 }
 
 function summElementsWorker(...arr) {
-
+  let summ = 0;
+  for (let i = 0; i < arr.length; i++)
+  sum += arr[i]
 }
 
 function differenceMaxMinWorker(...arr) {
+  let minElement = 0;
+  let maxElement = 0;
 
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    } else if (arr[i] < minElement) {
+      minElement = arr[i];
+    }
+  }
 }
 
 function differenceEvenOddWorker(...arr) {
-
+  let sumEvenElement = 0; 
+  let sumOddElement = 0;
+  
+  for (let i = 0; i <arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+    } else {
+      sumOddElement += arr[i];
+    }
+  }
+  return sumEvenElement - sumOddElement;
 }
 
 function averageEvenElementsWorker(...arr) {
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+      countEvenElement++
+    }
+  }
+  
+  if (countEvenElement === 0) {
+    return 0;
+  } else {
+    return sumEvenElement / countEvenElement;
+  }
 }
 
 function makeWork (arrOfArr, func) {
