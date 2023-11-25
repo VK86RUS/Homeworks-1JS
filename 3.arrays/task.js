21645749e9ea034a25201fd6548dac7e0f2b38b8
BUBA <buba86@ro.ru>
2023-11-25 16:10:55 +0500

    3 задача

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,16 @@
+// Задача 1. Сравнить массивы
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    const result = arr1.every((value, index) => value === arr2[index]);
 }
 
+// Задача 2. Фильтрация и преобразование массива
 function getUsersNamesInAgeRange(users, gender) {
-  
+    const filterUsers = users.filter(user => user.gender === gender);
+    const age = filterUsers.map(user =>user.age);
+    const averageAge = age.reduce((sum, age) => sum + age, 0) / age.length;
+    
+    return averageAge
 }
