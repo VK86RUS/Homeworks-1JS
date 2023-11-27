64559df641e5e51dd4738c5ca8130c36b458307d
BUBA <buba86@ro.ru>
2023-11-27 21:36:25 +0500

    3 задача/

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -4,13 +4,17 @@ function compareArrays(arr1, arr2) {
         return false;
     }
     const result = arr1.every((value, index) => value === arr2[index]);
+    console.log(result)
 }
 
 // Задача 2. Фильтрация и преобразование массива
 function getUsersNamesInAgeRange(users, gender) {
     const filterUsers = users.filter(user => user.gender === gender);
+    if (filterUsers.length === 0) {
+        return 0
+    }
+
     const age = filterUsers.map(user =>user.age);
     const averageAge = age.reduce((sum, age) => sum + age, 0) / age.length;
-    
     return averageAge
 }
