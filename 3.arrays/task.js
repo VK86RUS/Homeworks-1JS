899f0676908b92fa8c9be5a78650131597d0891b
BUBA <buba86@ro.ru>
2023-12-02 14:45:01 +0500

    3 задача выполнена.

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -3,8 +3,11 @@ function compareArrays(arr1, arr2) {
     if (arr1.length !== arr2.length) {
         return false;
     }
-    const result = arr1.every((value, index) => value === arr2[index]);
-    console.log(result)
+    return arr1.every((value, index) => {
+        console.log(value, arr2[index]);
+        return value === arr2[index];
+    }
+    )
 }
 
 // Задача 2. Фильтрация и преобразование массива
