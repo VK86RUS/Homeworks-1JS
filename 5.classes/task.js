de608e05367c77e45d1ae7a818d9709888280ec0
VK86RUS <136136995+VK86RUS@users.noreply.github.com>
2023-12-13 10:33:04 +0500

    5 задание ок

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,33 +1,31 @@
 class PrintEditionItem {
-    constructor(name, releaseDate, pagesCount) {
-      this.name = name;
-      this.releaseDate = releaseDate;
-      this.pagesCount = pagesCount;
-      this.state = 100;
-      this.type = null;
-    }
-    fix() {
-      this.state = 1.5;
-    }
-    set state(state) {
-      if(state < 0) {
-        this._state = 0;
-      
-  } else if(state > 100) {
-        this._state = 100;
-      }
-      else {
-        this._state = state;
-      }
-    }
-    get state() {
-      return this._state;
-    }
+  constructor(name, releaseDate, pagesCount) {
+    this.name = name;
+    this.releaseDate = releaseDate;
+    this.pagesCount = pagesCount;
+    this.state = 100;
+    this.type = null;
 }
-    class Magazine extends PrintEditionItem {
-    constructor(name, releaseDate, pagesCount) {
-      super(name, releaseDate, pagesCount);
-      this.type = "magazine";
+  fix() {
+    this.state *= 1.5;
+  }
+  set state(newState) {
+    if (newState < 0) {
+      this._state = 0;
+    } else if (newState > 100) {
+      this._state = 100;
+    } else {
+      this._state = newState;
+    }
+  }
+  get state() {
+    return this._state;
+  }
+}
+class Magazine extends PrintEditionItem {
+  constructor(name, releaseDate, pagesCount) {
+  super(name, releaseDate, pagesCount);
+  this.type = "magazine";
   }
 }
 class Book extends PrintEditionItem {
